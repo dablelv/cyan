97c13eb51a6b2fc337d88c252f8b6f5b7e40b506
dablelv <dablelv@tencent.com>
2020-12-30 15:16:00 +0800

    simplify the function VerGTVer VerLTVer VerGEVer VerLEVer

--- vercmp.go
+++ vercmp.go
@@ -2,96 +2,39 @@
 // Your version number should be MAJOR.MINOR.PATCH e.g. 1.0.0.
 // For more version number details refer to Semantic Versioning 2.0.0(https://semver.org/).
 // Note all of the functions don't support pre-release version, e.g. 1.0.0-alpha.
-// If your version number seperator isn't dot or the length you want to compare isn't three,
+// If your version number separator isn't dot or the length you want to compare isn't three,
 // please specify separator and length.
 
 package util
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 )
 
 // VerGTVer determines whether version0 greater than version1
-// Version number is MAJOR.MINOR.PATCH, e.g. 2.1.1 > 2.1.0 > 2.0.0 > 1.0.0
+// Version number like MAJOR.MINOR.PATCH, e.g. 2.1.1 > 2.1.0 > 2.0.0 > 1.0.0
 func VerGTVer(ver0, ver1 string) (bool, error) {
-	ver0, ver1 = strings.TrimSpace(ver0), strings.TrimSpace(ver1)
-	if ver0 == ver1 {
-		return false, nil
-	}
-
-	slVer0, slVer1 := Split(ver0, "."), Split(ver1, ".")
-	if len(slVer0) != len(slVer1) {
-		return false, errors.New("param error")
-	}
-
-	var i64Ver0, i64Ver1 int64
-	for i := 0; i < len(slVer0); i++ {
-		v0, err0 := ToInt64E(slVer0[i])
-		if err0 != nil {
-			return false, errors.New("the first version number is ill")
-		}
-		v1, err1 := ToInt64E(slVer1[i])
-		if err1 != nil {
-			return false, errors.New("the second version number is ill")
-		}
-		i64Ver0 *= 10
-		i64Ver0 += v0
-		i64Ver1 *= 10
-		i64Ver1 += v1
-	}
-	return i64Ver0 > i64Ver1, nil
+	return VerGTVerMore(ver0, ver1, ".", 3)
 }
 
 // VerLTVer determines whether version0 less than version1
-// Version number is MAJOR.MINOR.PATCH, e.g. 1.0.0 < 2.0.0 < 2.1.0 < 2.1.1
+// Version number like MAJOR.MINOR.PATCH, e.g. 1.0.0 < 2.0.0 < 2.1.0 < 2.1.1
 func VerLTVer(ver0, ver1 string) (bool, error) {
-	ver0, ver1 = strings.TrimSpace(ver0), strings.TrimSpace(ver1)
-	if ver0 == ver1 {
-		return false, nil
-	}
-
-	slVer0, slVer1 := Split(ver0, "."), Split(ver1, ".")
-	if len(slVer0) != len(slVer1) {
-		return false, errors.New("param error")
-	}
-
-	var i64Ver0, i64Ver1 int64
-	for i := 0; i < len(slVer0); i++ {
-		v0, err0 := ToInt64E(slVer0[i])
-		if err0 != nil {
-			return false, errors.New("the first version number is ill")
-		}
-		v1, err1 := ToInt64E(slVer1[i])
-		if err1 != nil {
-			return false, errors.New("the second version number is ill")
-		}
-		i64Ver0 *= 10
-		i64Ver0 += v0
-		i64Ver1 *= 10
-		i64Ver1 += v1
-	}
-	return i64Ver0 < i64Ver1, nil
+	return VerLTVerMore(ver0, ver1, ".", 3)
 }
 
 // VerGEVer determines whether version0 greater than or equal to version1
-// Version number is MAJOR.MINOR.PATCH, e.g. 2.1.1 > 2.1.0 > 2.0.0 > 1.0.0
+// Version number like MAJOR.MINOR.PATCH, e.g. 2.1.1 > 2.1.0 > 2.0.0 > 1.0.0
 func VerGEVer(ver0, ver1 string) (bool, error) {
-	ver0, ver1 = strings.TrimSpace(ver0), strings.TrimSpace(ver1)
-	if ver0 == ver1 {
-		return true, nil
-	}
-	return VerGTVer(ver0, ver1)
+	return VerGEVerMore(ver0, ver1, ".", 3)
 }
 
 // VerLEVer determines whether version0 less than or equal to version1
-// Version number is MAJOR.MINOR.PATCH, e.g. 1.0.0 < 2.0.0 < 2.1.0 < 2.1.1
+// Version number like MAJOR.MINOR.PATCH, e.g. 1.0.0 < 2.0.0 < 2.1.0 < 2.1.1
 func VerLEVer(ver0, ver1 string) (bool, error) {
-	ver0, ver1 = strings.TrimSpace(ver0), strings.TrimSpace(ver1)
-	if ver0 == ver1 {
-		return true, nil
-	}
-	return VerLTVer(ver0, ver1)
+	return VerLEVerMore(ver0, ver1, ".", 3)
 }
 
 // VerGTVerMore determines whether version0 greater than version1 with specified version separator and length
@@ -103,8 +46,8 @@ func VerGTVerMore(ver0, ver1, sep string, num int) (bool, error) {
 	}
 
 	slVer0, slVer1 := Split(ver0, sep), Split(ver1, sep)
-	if len(slVer0) < num || len(slVer1) < num {
-		return false, errors.New("version field num is too short")
+	if len(slVer0) != num || len(slVer1) != num {
+		return false, errors.New(fmt.Sprintf("version field num is not equal to %v", num))
 	}
 
 	var i64Ver0, i64Ver1 int64
@@ -134,7 +77,7 @@ func VerLTVerMore(ver0, ver1, sep string, num int) (bool, error) {
 	}
 
 	slVer0, slVer1 := Split(ver0, sep), Split(ver1, sep)
-	if len(slVer0) < num || len(slVer1) < num {
+	if len(slVer0) != num || len(slVer1) != num {
 		return false, errors.New("version field num is too short")
 	}
 
