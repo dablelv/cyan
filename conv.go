8e3cec2ffb17a35a2a4c2f44f937daab0ae06895
dablelv <dablelv@tencent.com>
2021-10-18 20:07:56 +0800

    add some converting function about to map set

--- conv.go
+++ conv.go
@@ -1,7 +1,7 @@
 package util
 
 import (
-	"errors"
+	"fmt"
 	"reflect"
 
 	"github.com/spf13/cast"
@@ -11,7 +11,7 @@ import (
 func Map2SliceE(i interface{})([]interface{}, []interface{}, error) {
 	kind := reflect.TypeOf(i).Kind()
 	if kind != reflect.Map {
-		return nil, nil, errors.New("the input is not a map")
+		return nil, nil, fmt.Errorf("the input %#v of type %T isn't a map", i, i)
 	}
 	m := reflect.ValueOf(i)
 	keys := m.MapKeys()
@@ -25,24 +25,64 @@ func Map2SliceE(i interface{})([]interface{}, []interface{}, error) {
 }
 
 // Map2StrSliceE converts keys and values of map to string slice with error
-func Map2StrSliceE(i interface{})([]string, []string, error) {
+func Map2StrSliceE(i interface{})(k []string, v []string, err error) {
 	slK, slV, err := Map2SliceE(i)
 	if err != nil {
-		return nil, nil, err
+		return
 	}
-	k, err := cast.ToStringSliceE(slK)
+	k, err = cast.ToStringSliceE(slK)
 	if err != nil {
-		return nil, nil, err
+		return
 	}
-	v, err := cast.ToStringSliceE(slV)
-	if err != nil {
-		return nil, nil, err
-	}
-	return k, v, nil
+	v, err = cast.ToStringSliceE(slV)
+	return
 }
 
 // Map2StrSlice converts keys and values of map to string slice
 func Map2StrSlice(i interface{})([]string, []string) {
 	slK, slV, _ := Map2StrSliceE(i)
 	return slK, slV
+}
+
+// ToMapSetE converts a slice or array to map[interface{}]struct{} with error
+func ToMapSetE(i interface{}) (map[interface{}]struct{}, error) {
+	// judge the validation of the input
+	if i == nil {
+		return nil, fmt.Errorf("unable to converts %#v of type %T to map[interface{}]struct{}", i, i)
+	}
+	kind := reflect.TypeOf(i).Kind()
+	if kind != reflect.Slice && kind != reflect.Array {
+		return nil, fmt.Errorf("the input %#v of type %T isn't a slice or array", i, i)
+	}
+
+	// execute the convert
+	v := reflect.ValueOf(i)
+	m := make(map[interface{}]struct{}, v.Len())
+	for j := 0; j < v.Len(); j++ {
+		m[v.Index(j).Interface()] = struct{}{}
+	}
+	return m, nil
+}
+
+// ToStrMapSetE converts a slice or array to map[string]struct{} with error
+func ToStrMapSetE(i interface{}) (map[string]struct{}, error) {
+	m, err := ToMapSetE(i)
+	if err != nil {
+		return nil, err
+	}
+	mm := make(map[string]struct{}, len(m))
+	for k := range m {
+		kk, err := cast.ToStringE(k)
+		if err != nil {
+			return nil, err
+		}
+		mm[kk] = struct{}{}
+	}
+	return mm, nil
+}
+
+// ToStrMapSet converts a slice or array to map[string]struct{}
+func ToStrMapSet(i interface{}) map[string]struct{}{
+	m, _ := ToStrMapSetE(i)
+	return m
 }
--- conv_test.go
+++ conv_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -51,6 +52,92 @@ func TestMap2StrSliceE(t *testing.T) {
 			assert.Equal(t, tt.wantErr, err != nil, tt.name)
 			assert.Equal(t, len(tt.want), len(got), tt.name)
 			assert.Equal(t, len(tt.want1), len(got1), tt.name)
+		})
+	}
+}
+
+func TestToMapSetE(t *testing.T) {
+	type args struct {
+		i interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[interface{}]struct{}
+		wantErr bool
+	}{
+		{
+			name:"[]string",
+			args:args{[]string{"CN","HK","AU"}},
+			want:map[interface{}]struct{}{"CN":struct{}{},"HK":struct{}{},"AU":struct{}{}},
+			wantErr: false,
+		},
+		{
+			name:"[3]int",
+			args:args{[3]int{86,852,61}},
+			want:map[interface{}]struct{}{86:struct{}{},852:struct{}{},61:struct{}{}},
+			wantErr: false,
+		},
+		{
+			name:"string",
+			args:args{""},
+			want:nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ToMapSetE(tt.args.i)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ToMapSetE() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToMapSetE() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestToStrMapSetE(t *testing.T) {
+	type args struct {
+		i interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]struct{}
+		wantErr bool
+	}{
+		{
+			name:"[]string",
+			args:args{[]string{"CN","HK","AU"}},
+			want:map[string]struct{}{"CN":struct{}{},"HK":struct{}{},"AU":struct{}{}},
+			wantErr: false,
+		},
+		{
+			name:"[3]int",
+			args:args{[3]int{86,852,61}},
+			want:map[string]struct{}{"86":struct{}{},"852":struct{}{},"61":struct{}{}},
+			wantErr: false,
+		},
+		{
+			name:"string",
+			args:args{""},
+			want:nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ToStrMapSetE(tt.args.i)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ToStrMapSetE() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToStrMapSetE() got = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
