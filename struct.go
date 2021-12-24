d6286458efea4196dca0c588496e82601d077664
dablelv <dablelv@tencent.com>
2021-12-24 14:25:20 +0800

    add function comments and unit test

--- struct.go
+++ struct.go
@@ -6,24 +6,42 @@ import (
 	"github.com/spf13/cast"
 )
 
+// Struct2Map converts struct to map[string]interface{}.
+// Such as struct{I int, S string}{I: 1, S: "a"} to map[I:1 S:a].
+// Note that unexported fields of struct can't be converted.
 func Struct2Map(obj interface{}) map[string]interface{} {
-	t := reflect.TypeOf(obj)
+	// check params
 	v := reflect.ValueOf(obj)
+	if v.Kind() != reflect.Struct {
+		return nil
+	}
 
+	t := reflect.TypeOf(obj)
 	var m = make(map[string]interface{})
 	for i := 0; i < t.NumField(); i++ {
-		m[t.Field(i).Name] = v.Field(i).Interface()
+		if t.Field(i).IsExported() {
+			m[t.Field(i).Name] = v.Field(i).Interface()
+		}
 	}
 	return m
 }
 
+// Struct2MapString converts struct to map[string]string.
+// Such as struct{I int, S string}{I: 1, S: "a"} to map[I:1 S:a].
+// Note that unexported fields of struct can't be converted.
 func Struct2MapString(obj interface{}) map[string]string {
-	t := reflect.TypeOf(obj)
+	// check params
 	v := reflect.ValueOf(obj)
+	if v.Kind() != reflect.Struct {
+		return nil
+	}
 
+	t := reflect.TypeOf(obj)
 	var m = make(map[string]string)
 	for i := 0; i < t.NumField(); i++ {
-		m[t.Field(i).Name] = cast.ToString(v.Field(i).Interface())
+		if t.Field(i).IsExported() {
+			m[t.Field(i).Name] = cast.ToString(v.Field(i).Interface())
+		}
 	}
 	return m
 }
---
+++ struct_test.go
@@ -0,0 +1,92 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStruct2Map(t *testing.T) {
+	type args struct {
+		obj interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]interface{}
+	}{
+		{
+			name:"convert success",
+			args:args{
+			struct{
+				I int
+				S string
+			}{I: 1, S: "a"}},
+			want:map[string]interface{}{"I":1,"S":"a"},
+		},
+		{
+			name:"convert struct contains unexported fields success",
+			args:args{
+				struct{
+					I int
+					S string
+					s string
+				}{I: 1, S: "a", s:"a"}},
+			want:map[string]interface{}{"I":1,"S":"a"},
+		},
+		{
+			name:"convert empty struct success",
+			args:args{struct{}{}},
+			want:map[string]interface{}{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Struct2Map(tt.args.obj); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Struct2Map() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStruct2MapString(t *testing.T) {
+	type args struct {
+		obj interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name:"convert success",
+			args:args{
+				struct{
+					I int
+					S string
+				}{I: 1, S: "a"}},
+			want:map[string]string{"I":"1","S":"a"},
+		},
+		{
+			name:"convert struct contains unexported fields success",
+			args:args{
+				struct{
+					I int
+					S string
+					s string
+				}{I: 1, S: "a", s:"a"}},
+			want:map[string]string{"I":"1","S":"a"},
+		},
+		{
+			name:"convert empty struct success",
+			args:args{struct{}{}},
+			want:map[string]string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Struct2MapString(tt.args.obj); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Struct2MapString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
