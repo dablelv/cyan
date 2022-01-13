fd3c3a291a9f175eb02647a2af463e462c3e83ba
dablelv <dablelv@tencent.com>
2022-01-13 21:12:22 +0800

    add comments

--- abs.go
+++ abs.go
@@ -2,50 +2,47 @@ package util
 
 // AbsInt8 gets absolute value of int8.
 //
-// Example 1 : AbsInt8(-5)
-// -5 的表示
-// 原码 1000,0101
-// 反码 1111,1010
-// 补码 1111,1011
-// 负数在内存中以补码的形式表示
-// shifted = n >> 7 = (1111,1011) >> 7 = 1111,1111 = -1(十进制) (负数右移，左补1)
+// Example 1: AbsInt8(-5)
+// -5 code value as below
+// original code	1000,0101
+// inverse code		1111,1010
+// complement code	1111,1011
+// Negative numbers are represented by complement code in memory.
+// shifted = n >> 7 = (1111,1011) >> 7 = 1111,1111 = -1(10-base) (负数右移，左补1)
 //               1111,1011
-// n xor shifted =  ----------- = 0000,0100 = 4(十进制)
+// n xor shifted =  ----------- = 0000,0100 = 4(10-base)
 //               1111,1111
 // (n ^ shifted) - shifted = 4 - (-1) = 5
 //
-// Example 2:  AbsInt8(5)
-// 5 的表示
-// 原码 0000,0101
-// 正数在内存中以原码的形式表示，而正数与 0 进行异或运算等于本身
+// Example 2: AbsInt8(5)
+// 5 code value as below
+// original code 0000,0101
+// Positive numbers are represented by original code in memory,
+// and the XOR operation between positive numbers and 0 is equal to itself.
 // shifted = n >> 7 = 0
 //               0000,0101
-// n xor shifted =  ----------- = 0000,0101 = 5(十进制)
+// n xor shifted =  ----------- = 0000,0101 = 5(10-base)
 //               0000,0000
 // (n ^ shifted) - shifted = 5 - 0 = 5
 func AbsInt8(n int8) int8 {
-	// n 右移 7 位
 	shifted := n >> 7
 	return (n ^ shifted) - shifted
 }
 
-// AbsInt16 int16 取绝对值
+// AbsInt16 gets absolute value of int16.
 func AbsInt16(n int16) int16 {
-	// n 右移 15 位
 	shifted := n >> 15
 	return (n ^ shifted) - shifted
 }
 
-// AbsInt32 int32 取绝对值
+// AbsInt32 gets absolute value of int32.
 func AbsInt32(n int32) int32 {
-	// n 右移 31 位
 	shifted := n >> 31
 	return (n ^ shifted) - shifted
 }
 
-// AbsInt64 int64 取绝对值
+// AbsInt64 gets absolute value of int64.
 func AbsInt64(n int64) int64 {
-	// n 右移 63 位
 	shifted := n >> 63
 	return (n ^ shifted) - shifted
 }
