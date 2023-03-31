7ff4aabb64ec5c652a13b19c0708328659f41669
R22627 <lvlv R22627/autelrobotics>
2023-03-31 09:36:55 +0800

    Add more unit tests.

--- slice/make.go
+++ slice/make.go
@@ -4,8 +4,8 @@ package slice
 // The first integer parameter represents the slice length.
 // A second integer argument may be provided to specify a different capacity.
 // If there is no integer parameter, MakeSlice will return a slice only include one specified element.
-// Which means that MakeSlice(val) is equal to MakeSlice(val, 1).
-func MakeSlice[T any](val T, size ...int) []T {
+// Which means that MakeSlice(v) is equal to MakeSlice(v, 1).
+func MakeSlice[T any](v T, size ...int) []T {
 	var slice []T
 	switch l := len(size); {
 	case l >= 2:
@@ -13,10 +13,10 @@ func MakeSlice[T any](val T, size ...int) []T {
 	case l >= 1:
 		slice = make([]T, size[0])
 	default:
-		return []T{val}
+		return []T{v}
 	}
 	for i := 0; i < size[0]; i++ {
-		slice[i] = val
+		slice[i] = v
 	}
 	return slice
 }
