f04a1bfd8a52dbe224c8306cc55d4a2e209f2478
R22627 <lvlv R22627/autelrobotics>
2023-11-14 08:57:08 +0800

    feat: add some string word utility functions

---
+++ str/word.go
@@ -0,0 +1,109 @@
+package str
+
+import (
+	"bytes"
+	"strings"
+	"unicode"
+)
+
+/*
+Capitalize capitalizes all the delimiter separated words in a string. Only the first letter of each word is changed.
+To convert the rest of each word to lowercase at the same time, use CapitalizeFully(str string, delimiters ...rune).
+The delimiters represent a set of characters understood to separate words.The first string character and the first
+non-delimiter character after a delimiter will be capitalized.Capitalization uses the Unicode title case,
+normally equivalent to upper case.
+*/
+func Capitalize(s string, delimiters ...rune) string {
+	if len(delimiters) == 0 {
+		delimiters = []rune{' '}
+	}
+
+	buffer := []rune(s)
+	capitalizeNext := true
+	for i := 0; i < len(buffer); i++ {
+		ch := buffer[i]
+		if isDelimiter(ch, delimiters...) {
+			capitalizeNext = true
+		} else if capitalizeNext {
+			buffer[i] = unicode.ToTitle(ch)
+			capitalizeNext = false
+		}
+	}
+	return string(buffer)
+}
+
+/*
+CapitalizeFully converts all the delimiter separated words in a string into capitalized words, that is each word is made up of a
+titlecase character and then a series of lowercase characters. The delimiters represent a set of characters understood to separate words.
+The first string character and the first non-delimiter character after a delimiter will be capitalized.
+Capitalization uses the Unicode title case, normally equivalent to upper case.
+*/
+func CapitalizeFully(s string, delimiters ...rune) string {
+	if len(delimiters) == 0 {
+		delimiters = []rune{' '}
+	}
+
+	s = strings.ToLower(s)
+	return Capitalize(s, delimiters...)
+}
+
+/*
+Uncapitalize uncapitalizes all the whitespace separated words in a string. Only the first letter of each word is changed.
+The delimiters represent a set of characters understood to separate words. The first string character and the first non-delimiter
+character after a delimiter will be uncapitalized. Whitespace is defined by unicode.IsSpace(char).
+*/
+func Uncapitalize(str string, delimiters ...rune) string {
+	if len(delimiters) == 0 {
+		delimiters = []rune{' '}
+	}
+
+	buffer := []rune(str)
+	uncapitalizeNext := true
+	for i := 0; i < len(buffer); i++ {
+		ch := buffer[i]
+		if isDelimiter(ch, delimiters...) {
+			uncapitalizeNext = true
+		} else if uncapitalizeNext {
+			buffer[i] = unicode.ToLower(ch)
+			uncapitalizeNext = false
+		}
+	}
+	return string(buffer)
+}
+
+/*
+Initials extracts the initial letters from each word in the string. The first letter of the string and all first
+letters after the defined delimiters are returned as a new string. Their case is not changed. If the delimiters
+parameter is excluded, then Whitespace is used. Whitespace is defined by unicode.IsSpacea(char).
+*/
+func Initials(s string, delimiters ...rune) string {
+	if len(delimiters) == 0 {
+		delimiters = []rune{' '}
+	}
+
+	buffer := []rune(s)
+	var buf bytes.Buffer
+	lastWasGap := true
+	for i := 0; i < len(buffer); i++ {
+		ch := rune(s[i])
+		if isDelimiter(ch, delimiters...) {
+			lastWasGap = true
+		} else if lastWasGap {
+			buf.WriteRune(ch)
+			lastWasGap = false
+		}
+	}
+	return buf.String()
+}
+
+func isDelimiter(r rune, delimiters ...rune) bool {
+	if delimiters == nil {
+		return unicode.IsSpace(r)
+	}
+	for _, delimiter := range delimiters {
+		if r == delimiter {
+			return true
+		}
+	}
+	return false
+}
---
+++ str/word_test.go
@@ -0,0 +1,64 @@
+package str
+
+import (
+	"testing"
+
+	"github.com/dablelv/cyan/internal"
+)
+
+func TestCapitalize(t *testing.T) {
+	assert := internal.NewAssert(t, "TestCapitalize")
+
+	// Test 1: Checks empty string.
+	assert.Equal("", Capitalize(""))
+
+	// Test 2: Checks if function works with 1 parameter, and default whitespace delimiter.
+	in := "test is going.well.thank.you.for inquiring"
+	out := "Test Is Going.well.thank.you.for Inquiring"
+
+	assert.Equal(out, Capitalize(in))
+
+	// Test 3: Checks if function works with both parameters, with param 2 containing whitespace and '.'
+	out = "Test Is Going.Well.Thank.You.For Inquiring"
+	delimiters := []rune{' ', '.'}
+
+	assert.Equal(out, Capitalize(in, delimiters...))
+}
+
+func TestCapitalizeFully(t *testing.T) {
+	assert := internal.NewAssert(t, "CapitalizeFully")
+
+	assert.Equal("", CapitalizeFully(""))
+
+	in := "tEsT iS goiNG.wELL.tHaNk.yOU.for inqUIrING"
+
+	assert.Equal("Test Is Going.well.thank.you.for Inquiring", CapitalizeFully(in))
+
+	delimiters := []rune{' ', '.'}
+	assert.Equal("Test Is Going.Well.Thank.You.For Inquiring", CapitalizeFully(in, delimiters...))
+}
+
+func TestUncapitalize(t *testing.T) {
+	assert := internal.NewAssert(t, "Uncapitalize")
+
+	assert.Equal("", Uncapitalize(""))
+
+	in := "This Is A.Test"
+
+	assert.Equal("this is a.Test", Uncapitalize(in))
+
+	delimiters := []rune{' ', '.'}
+	assert.Equal("this is a.test", Uncapitalize(in, delimiters...))
+}
+
+func TestInitials(t *testing.T) {
+	assert := internal.NewAssert(t, "Initials")
+
+	assert.Equal("", Initials(""))
+
+	in := "John Doe.Ray"
+	assert.Equal("JD", Initials(in))
+
+	delimiters := []rune{' ', '.'}
+	assert.Equal("JDR", Initials(in, delimiters...))
+}
